df604cea04a149649a530113187d68f5c941a25b
ccastignani <44896853+ccastignani@users.noreply.github.com>
2019-03-05 12:36:01 +0100

    Add files via upload

--- users/users.php
+++ users/users.php
@@ -287,12 +287,6 @@ INNER JOIN Ordered ON Primer.Ordered_idOrdered = Ordered.idOrdered WHERE Primer.
 				</div>
 			</footer>
 
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
-
 	</body>
 </html>
 
